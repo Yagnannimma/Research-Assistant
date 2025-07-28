a536ef999b386b331ebecf03578d543a0ab85531
Yagnannimma <91213498+Yagnannimma@users.noreply.github.com>
2025-07-28 23:16:36 +0530

    Add files via upload
    Research Assistant

---
+++ src/main/java/com/research/assistant/ResearchController.java
@@ -0,0 +1,21 @@
+package com.research.assistant;
+
+
+import lombok.AllArgsConstructor;
+import org.apache.coyote.Response;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/resources")
+@CrossOrigin(origins="*")
+@AllArgsConstructor
+public class ResearchController {
+    private final ResearchService researchService;
+
+    @PostMapping("/process")
+    public ResponseEntity<String> processContent(@RequestBody ResearchRequest researchRequest){
+        String result = researchService.processContent(researchRequest);
+        return ResponseEntity.ok(result);
+    }
+}
